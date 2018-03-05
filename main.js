85a7561fc0d6411d3436a951dad82038d78a4cdf
Joshuah <jcharney95@gmail.com>
2018-03-05 03:15:46 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const fetch = require('axios');
+const Bacon = require('baconjs');
+const translate = require('google-translate-api');
+
+function getInGerman(word) {
+    const promise = translate(word, {to: 'german'});
+
+    const stream = Bacon.fromPromise(promise);
+    return stream
+}
+
+const stream = new Bacon.Bus();
+
+stream
+    .flatMap(word => getInGerman(word))
+    .onValue(word => console.log(word.text));
+
+stream.push('engineering');
+stream.push('dog');
+stream.push('steak');
